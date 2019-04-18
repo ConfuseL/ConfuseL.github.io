371477dfe90f349375f10c00a9f1638c3da90d08
ConfuseL <895210069@qq.com>
2019-04-18 19:52:24 +0800

    Site updated: 2019-04-18 19:52:21

--- js/src/crash_cheat.js
+++ js/src/crash_cheat.js
@@ -3,12 +3,12 @@
  var titleTime;
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
-         $('[rel="icon"]').attr('href', "/img/TEP.ico");
+         $('[rel="icon"]').attr('href', "/images/64.png");
          document.title = '深渊凝视着你';
          clearTimeout(titleTime);
      }
      else {
-         $('[rel="icon"]').attr('href', "/favicon.ico");
+         $('[rel="icon"]').attr('href', "/images/64.png");
          document.title = 'ConfuseL的个人博客' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
